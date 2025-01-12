643bf6ff88076aff4a140d8860f64793c52af8c6
Drew Belloc <bellocdrew@proton.me>
2025-01-12 15:35:08 -0300

    changed fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    . /usr/share/autojump/autojump.fish
 end
 
 function fish_prompt
-    printf '%s%s%s%s%s %s%s %s%s%s ' (set_color -b magenta) (set_color black) " Belloc " (set_color magenta) (set_color -b blue) \
-        (set_color black) (prompt_pwd) (set_color -b normal) (set_color blue) (set_color normal)
+    printf '%s%s%s%s%s%s   %s %s%s%s ' (set_color -b red) (set_color black) " Belloc " (set_color red) (set_color -b cyan) (set_color black) (prompt_pwd) (set_color -b normal) (set_color cyan) (set_color normal)
+end
+
+function fish_right_prompt -d "Write out the right prompt"
+    printf '%s%s%s %s%s%s%s' (set_color magenta) (set_color -b magenta) (set_color black) (date '+%d/%m') (echo " 󰃭  ") (set_color normal) (set_color -b normal)
+end
+
+function fish_greeting
 end
