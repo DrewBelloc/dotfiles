6021ed4f0d9de09a9b4bb3280542fd9d7be2b741
Drew Belloc <bellocdrew@proton.me>
2025-01-12 14:48:45 -0300

    fish powerline

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    . /usr/share/autojump/autojump.fish
+end
+
+function fish_prompt
+    printf '%s%s%s%s%s %s%s %s%s%s ' (set_color -b magenta) (set_color black) " Belloc " (set_color magenta) (set_color -b blue) \
+        (set_color black) (prompt_pwd) (set_color -b normal) (set_color blue) (set_color normal)
 end
