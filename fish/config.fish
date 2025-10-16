d08ac0aaf6babd196fa1c6747ae3079be6d4f01d
Drew Belloc <bellocdrew@proton.me>
2025-10-16 20:18:12 -0300

    Too many changes to detail

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,38 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    . /usr/share/autojump/autojump.fish
 end
+
+set -g fish_key_bindings fish_vi_key_bindings
+set -g VIRTUAL_ENV_DISABLE_PROMPT 1
 
 function fish_prompt
-    printf '%s%s   %s%s%s  %s %s%s%s ' (set_color -b red) (set_color black) (set_color red) (set_color -b cyan) (set_color black) (prompt_pwd) (set_color -b normal) (set_color cyan) (set_color normal)
+    set -l last_status $status
+    set -l stat
+    if test $last_status -ne 0
+        set stat (set_color red)" [$last_status]"
+        printf '%s%s %s %s%s%s%s %s%s%s ' (set_color -b black) (set_color white) (prompt_pwd) (set_color -b red) (set_color black) (set_color black) (echo $last_status) (set_color -b normal) (set_color red) (set_color normal)
+    else
+        printf '%s%s  %s %s%s%s ' (set_color -b black) (set_color white) (prompt_pwd) (set_color -b normal) (set_color black) (set_color normal)
+    end
 end
 
-function fish_right_prompt -d "Write out the right prompt"
-    printf '%s%s%s %s%s%s%s' (set_color magenta) (set_color -b magenta) (set_color black) (date '+%d/%m') (echo " 󰃭  ") (set_color normal) (set_color -b normal)
+function fish_right_prompt
+    if set -q VIRTUAL_ENV
+        printf '%s%s%s  %s %s %s%s' (set_color black) (set_color blue) (set_color -b black) (set_color white) (basename $VIRTUAL_ENV) (set_color normal) (set_color -b normal)
+
+    end
+end
+
+function fish_mode_prompt
+    switch $fish_bind_mode
+        case default
+            printf '%s%s   %s%s' (set_color -b magenta) (set_color black) (set_color magenta) (set_color -b black)
+        case insert
+            printf '%s%s   %s%s' (set_color -b blue) (set_color black) (set_color blue) (set_color -b black)
+        case visual
+            printf '%s%s 󰊪  %s%s' (set_color -b yellow) (set_color black) (set_color yellow) (set_color -b black)
+        case replace_one
+            printf '%s%s 󰬳  %s%s' (set_color -b red) (set_color black) (set_color red) (set_color -b black)
+    end
 end
 
 function fish_greeting
@@ -17,6 +41,3 @@ end
 function fuck
     eval command sudo $history[1]
 end
-
-alias yeet="sudo apt purge"
-alias up="sudo apt update && sudo apt upgrade -y && flatpak update -y"
